8ac59399f1d349e01398c6857926bc1467e20203
DoucheBagMIKE <mikemac440@gmail.com>
2018-02-26 09:39:25 -0500

    Initial commit

---
+++ Assets/ResetCar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class ResetCar : MonoBehaviour {
+
+    VRTK_ControllerEvents leftHand;
+    VRTK_ControllerEvents rightHand;
+
+    public GameObject ObjToReset;
+
+    Vector3 startLoc;
+    Quaternion startRot;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        leftHand = VRTK_SDKManager.instance.scriptAliasLeftController.GetComponent<VRTK_ControllerEvents>();
+        rightHand = VRTK_SDKManager.instance.scriptAliasRightController.GetComponent<VRTK_ControllerEvents>();
+
+        startLoc = ObjToReset.transform.position;
+        startRot = ObjToReset.transform.rotation;
+
+    }
+	
+	void Update ()
+    {
+        if(leftHand.touchpadPressed || rightHand.touchpadPressed)
+        {
+            ObjToReset.transform.position = startLoc;
+            ObjToReset.transform.rotation = startRot;
+        }
+		
+	}
+}
